b06ed85c6f6e1a546e1b361b2f2596850ec14e15
Mabende <belindamabende@gmail.com>
2024-05-05 07:09:27 +0200

    I am loved by my wife!

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.amberAccent,
+        appBar: AppBar(
+          backgroundColor: Colors.deepOrange,
+          title: Text('iSAD'),
+        ),
+      ),
+    ),
+  );
+}
